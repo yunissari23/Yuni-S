48d1c1474e4ffa62b165d27d596f1f5ad4d5bc2f
yunissari23 <158257065+yunissari23@users.noreply.github.com>
2024-02-21 13:39:18 +0700

    Update sari.py

--- sari.py
+++ sari.py
@@ -4,11 +4,11 @@ import matplotlib.pyplot as plt
 
 x = st.slider('Pilih rentang', 0.0, 2.0, (.2, .5))
 st.write('nilai x:', x)
-y = st.slider('Set nilai', 0.0, 100.0, 25.0)
+y = st.slider('Set nilai', 0.0, 10.0, 6.0)
 st.write('nilai y:', y)
 
 t = np.linspace(x[0]*np.pi,x[1]*np.pi,100)
-u = np.sin(t)
+u = np.sin(y*t)
 #st.write('nilai t:', t)
 
 fig, ax = plt.subplots(figsize=(16, 8))
