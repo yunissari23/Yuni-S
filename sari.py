c3b41be30a30e3d697ed76339ce945e65b911cf4
yunissari23 <158257065+yunissari23@users.noreply.github.com>
2024-02-21 13:25:21 +0700

    Create sari.py

---
+++ sari.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = st.slider('Pilih rentang', 0.0, 2.0, (.2, .5))
+st.write('nilai x:', x)
+y = st.slider('Set nilai', 0.0, 100.0, 25.0)
+st.write('nilai y:', y)
+
+t = np.linspace(x[0]*np.pi,x[1]*np.pi,100)
+u = np.sin(t)
+#st.write('nilai t:', t)
+
+fig, ax = plt.subplots(figsize=(16, 8))
+ax.plot(t, u, label='sin(t)', color='b') #Plotting sin(t) curve
+ax.set_ylabel("")
+ax.set_xlabel("t")
+ax.tick_params(axis='y', labelsize=20)
+ax.set_xticklabels(ax.get_xticklabels(), rotation=30, ha='right')
+ax.tick_params(axis='x', labelsize=15)
+plt.grid(color='green', linestyle='-.', linewidth=.5)
+st.pyplot(fig)
