6bc093b7eb2f3e6efd96fa83552a316f34e21d44
yunissari23 <158257065+yunissari23@users.noreply.github.com>
2024-02-21 14:12:06 +0700

    Update sari.py

--- sari.py
+++ sari.py
@@ -2,6 +2,12 @@ import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 
+def f(x):
+  a = 19
+  b = 1
+  c = 18
+  return a*x*x+b*x-c
+
 x = st.slider('Pilih rentang', 0.0, 2.0, (.2, .5))
 st.write('nilai x:', x)
 y = st.slider('Set nilai', 0.0, 10.0, 6.0)
@@ -11,6 +17,24 @@ t = np.linspace(x[0]*np.pi,x[1]*np.pi,100)
 u = np.sin(y*t)
 #st.write('nilai t:', t)
 
+fig, ax = plt.subplots(figsize=(16, 8))
+ax.plot(t, u, label='sin(t)', color='b') #Plotting sin(t) curve
+ax.set_ylabel("")
+ax.set_xlabel("t")
+ax.tick_params(axis='y', labelsize=20)
+ax.set_xticklabels(ax.get_xticklabels(), rotation=30, ha='right')
+ax.tick_params(axis='x', labelsize=15)
+plt.grid(color='green', linestyle='-.', linewidth=.5)
+st.pyplot(fig)
+
+#fungsi kuadrat
+def f(x):
+  a = 19
+  b = 1
+  c = 18
+  return a*x*x+b*x-c
+
+u = f(t)
 fig, ax = plt.subplots(figsize=(16, 8))
 ax.plot(t, u, label='sin(t)', color='b') #Plotting sin(t) curve
 ax.set_ylabel("")
