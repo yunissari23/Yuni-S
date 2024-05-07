77976880eae21ea8a14510fa358f6063c5176db7
yunissari23 <158257065+yunissari23@users.noreply.github.com>
2024-05-07 16:56:10 +0700

    Update prediksi_depresi.py

--- prediksi_depresi.py
+++ prediksi_depresi.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1rRzxGa2qG_PyAtbzugxNBlALVzOnmwQj
 """
 
-!pip install streamlit
 import gdown as gd
 import pandas as pd
 import numpy as np
