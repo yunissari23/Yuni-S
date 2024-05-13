6f0d017dde23fae0dae8e847cfa2d7445d086f98
yunissari23 <158257065+yunissari23@users.noreply.github.com>
2024-05-13 11:52:55 +0700

    Update prediksi_depresi.py

--- prediksi_depresi.py
+++ prediksi_depresi.py
@@ -75,7 +75,7 @@ st.write('')
 prediksi = st.button('Prediksi')
 
 col_names = ["Age", "Feeling sad", "Irritable towards people", "Trouble sleeping at night", "Problems concentrating or making decision", "loss of appetite", "Feeling of guilt", "Problems of bonding with people", "Suicide attempt"]
-df = pd.read_csv(file, sep='\t', index_col=0)
+df = pd.read_csv(file, index_col=0)
 
 # Split the data into features (X) and target variable (y)
 X = df.drop(columns=['Timestamp', 'Depressed'])
